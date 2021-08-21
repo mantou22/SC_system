c862ea082061706c8a86ea993ec3fa3279429e18
mt <ngrdaiwx@163.com>
2021-08-21 16:06:29 +0800

    修改数据表设计

--- srv/models.py
+++ srv/models.py
@@ -35,7 +35,7 @@ class sc_micro_course(models.Model):
     c_path = models.CharField(max_length=256, null=False, default='')  # 微课存放路径
     create_time = models.DateTimeField('date published')
     t_id = models.IntegerField(null=True, default=0)
-    c_desc = models.CharField(max_length=3000, null=True)
+    c_desc = models.CharField(max_length=3000, null=True) # 微课描述
     type = models.CharField(max_length=256, null=True)  # 微课类型
 
 
@@ -53,17 +53,17 @@ class stu_course(models.Model):
     process = models.CharField(max_length=256, null=True)
 
 
-class sc_model_essay(models.Model):
+class sc_model_essay(models.Model): # 模板范文
     id = models.AutoField(primary_key=True)
     es_id = models.IntegerField(null=True, default=0)  # 文章类型
-    title = models.CharField(max_length=256, null=True)
-    content = models.CharField(max_length=3000, null=True)
-    writing_theme = models.CharField(max_length=256, null=True)
-    tea_id = models.IntegerField(null=True, default=0)
-    create_time = models.DateTimeField('date published')
+    title = models.CharField(max_length=256, null=True) # 文章标题
+    content = models.CharField(max_length=3000, null=True) # 内容
+    writing_theme = models.CharField(max_length=256, null=True) # 写作主题
+    tea_id = models.IntegerField(null=True, default=0) # 老师id（谁发布的）
+    create_time = models.DateTimeField('date published') # 创建时间
 
 
-class sc_essay_style(models.Model):
+class sc_essay_style(models.Model): # 文章类型  （记叙、应用..）
     id = models.AutoField(primary_key=True)
     type_name = models.CharField(max_length=256, null=True)
 
@@ -71,15 +71,24 @@ class sc_essay_style(models.Model):
         return "id:" + str(self.id) + ", type_name:" + self.type_name
 
 
-class sc_writing_practice(models.Model):
-    id = models.AutoField(primary_key=True)
-    s_id = models.IntegerField(null=True, default=0)
-    wt_id = models.IntegerField(null=True, default=0)
-    content = models.CharField(max_length=3000, null=True)
-    finish_time = models.DateTimeField('date published')
-    notes = models.CharField(max_length=3000, null=False)
-    tea_score = models.CharField(max_length=256, null=False)
-    total_score = models.CharField(max_length=256, null=False)
-    machine_score = models.CharField(max_length=256, null=False)
+class sc_writing_task(models.Model): # 发布写作任务表
+    id = models.AutoField(primary_key=True)  # 主键
+    es_id = models.IntegerField(null=True, default=0)  # 文章类型
+    title = models.CharField(max_length=256, null=True) # 文章标题
+    requirement = models.CharField(max_length=256, null=True) # 写作要求
+    finish_time = models.DateTimeField('date published') # 截至时间
+
+
+class sc_stu_writing(models.Model): # 学生写作
+    id = models.AutoField(primary_key=True)  # 主键
+    wt_id = models.IntegerField(null=True, default=0)  # 写作任务id
+    s_id = models.IntegerField(null=True, default=0)  # 学生id
+    content = models.CharField(max_length=3000, null=True) # 内容
+    writing_theme = models.CharField(max_length=256, null=True) # 写作主题
+    notes = models.CharField(max_length=3000, null=False) # 老师评论
+    tea_score = models.CharField(max_length=256, null=False) # 老师评分
+    total_score = models.CharField(max_length=256, null=False) # 总分数
+    machine_score = models.CharField(max_length=256, null=False) # 机器评分
+    finish_time = models.DateTimeField('date published')  # 完成时间
     practice_type = models.IntegerField(null=True, default=0)  # 0--正式练习需要老师评分，1--自由练习--不需要老师评分，机器评分就行。
     is_upload = models.IntegerField(null=True, default=0)  # 是否上传。
